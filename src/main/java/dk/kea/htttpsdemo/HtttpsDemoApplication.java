94922e1e501b437702a6caca1e11e1514ccd410c
victorhanert <vict660m@stud.kea.dk>
2023-11-13 10:36:53 +0100

    first commit

---
+++ src/main/java/dk/kea/htttpsdemo/HtttpsDemoApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.htttpsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtttpsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtttpsDemoApplication.class, args);
+    }
+
+}
